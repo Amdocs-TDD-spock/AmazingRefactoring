976f50c9be2068d9675873b67e3a27e8749c6c51
MAHIEUX Ghislain <ghislain.mahieux@gmail.com>
2017-04-30 21:01:05 +0200

    Add more pseudo-random tests to improve refactoring reliability
    These tests have been successfully run on the original version.
    They are not all successfull now due to a bug in commits before this refactoring started
    The bug is that in some case the maze has no exit (ie seed=50 & size = 4x5)

--- test/TestAmazing.java
+++ test/TestAmazing.java
@@ -1,161 +1,69 @@
 // Copyright 2003, William C. Wake. All rights reserved.
 
 import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Random;
 
+@RunWith(Parameterized.class)
 public class TestAmazing extends TestCase {
-    public void testSeed0size15x20() {
-        String expected =
-                "Amazing - Copyright by Creative Computing, Morristown, NJ\n" +
-                ":--:--:--:--:--:--:--:--:--:--:  :--:--:--:--:\n" +
-                "I  I           I        I     I     I        I \n" +
-                ":  :  :  :  :  :  :--:  :  :  :  :--:  :--:  :\n" +
-                "I     I  I  I  I     I     I     I     I  I  I \n" +
-                ":--:--:  :  :--:  :  :--:--:--:--:  :  :  :  :\n" +
-                "I     I  I        I  I              I  I     I \n" +
-                ":  :  :  :--:--:--:  :  :  :--:--:--:--:--:--:\n" +
-                "I  I     I        I     I  I     I        I  I \n" +
-                ":--:--:  :--:  :--:  :--:  :  :  :  :--:  :  :\n" +
-                "I     I        I        I  I  I     I     I  I \n" +
-                ":  :  :--:--:--:  :--:  :  :  :--:--:  :--:  :\n" +
-                "I  I              I     I  I  I     I  I     I \n" +
-                ":  :--:--:--:--:--:--:  :  :  :  :--:  :--:  :\n" +
-                "I        I           I  I  I  I     I  I     I \n" +
-                ":  :--:  :--:  :  :  :  :  :  :--:  :  :  :--:\n" +
-                "I  I     I     I  I  I  I  I     I  I  I  I  I \n" +
-                ":  :  :--:  :--:  :  :  :  :--:  :  :  :  :  :\n" +
-                "I  I        I     I  I  I        I  I  I  I  I \n" +
-                ":  :--:  :--:  :--:  :  :--:--:--:  :  :  :  :\n" +
-                "I     I     I  I  I  I  I     I        I     I \n" +
-                ":--:  :--:  :  :  :  :--:  :  :  :--:--:  :--:\n" +
-                "I  I     I  I     I     I  I  I     I  I     I \n" +
-                ":  :--:  :--:--:--:  :  :  :  :--:  :  :  :  :\n" +
-                "I     I     I     I  I  I  I     I  I  I  I  I \n" +
-                ":  :  :--:  :  :  :--:  :  :--:  :  :  :  :  :\n" +
-                "I  I        I  I     I  I     I     I  I  I  I \n" +
-                ":  :--:--:  :--:  :  :  :--:  :--:--:  :  :--:\n" +
-                "I  I     I        I  I     I     I     I     I \n" +
-                ":  :  :--:--:--:--:  :  :--:  :  :  :--:--:  :\n" +
-                "I     I           I  I     I  I  I  I     I  I \n" +
-                ":  :--:  :--:--:--:  :--:  :  :  :  :  :  :  :\n" +
-                "I  I                 I     I  I  I     I  I  I \n" +
-                ":  :  :--:--:--:--:--:  :--:  :--:  :--:  :  :\n" +
-                "I  I  I  I           I     I     I  I     I  I \n" +
-                ":  :  :  :  :--:  :  :--:  :--:  :  :  :--:  :\n" +
-                "I  I  I     I     I  I  I     I     I  I     I \n" +
-                ":--:  :--:--:  :  :  :  :  :  :--:--:  :--:  :\n" +
-                "I     I        I  I  I  I  I  I     I        I \n" +
-                ":  :--:  :--:--:  :  :  :  :  :--:  :  :--:--:\n" +
-                "I        I        I     I  I        I        I \n" +
-                ":--:--:--:--:--:--:--:--:  :--:--:--:--:--:--:\n";
 
-        Amazing.random = new Random(0);
-        Amazing.doit(15, 20);
+    @Parameterized.Parameter(0)
+    public int seed;
 
-        assertEquals("Should have the maze that was expected", expected, Amazing.result.toString());
+    @Parameterized.Parameter(1)
+    public int colCount;
+
+    @Parameterized.Parameter(2)
+    public int rowCount;
+
+
+    @Parameterized.Parameters(name = "Test maze {1}x{2} with seed {0}")
+    public static Collection<Object[]> data() {
+        List<Object[]> data = new ArrayList<>();
+        Random rnd = new Random(27);
+        data.add(new Object[]{50,4,5});
+        for (int colCount = 1; colCount <= 30; colCount += 4) {
+            for (int rowCount = 1; rowCount <= 30; rowCount += 3) {
+                for (int seedIndex = 0; seedIndex < 5; seedIndex ++) {
+
+                    data.add(new Object[]{rnd.nextInt(15000), colCount, rowCount});
+                }
+            }
+        }
+        return data;
     }
 
-    public void testSeed42size20x20() {
-        String expected =
-                "Amazing - Copyright by Creative Computing, Morristown, NJ\n" +
-                        ":--:--:--:--:--:--:--:--:--:--:--:--:--:--:  :--:--:--:--:--:\n" +
-                        "I                 I           I              I              I \n" +
-                        ":--:--:--:--:  :  :  :--:--:  :  :--:--:  :  :--:  :--:  :--:\n" +
-                        "I              I  I        I     I        I     I     I     I \n" +
-                        ":  :--:  :--:--:  :--:--:  :--:--:  :  :--:--:  :--:  :--:  :\n" +
-                        "I     I  I     I     I        I     I  I     I        I  I  I \n" +
-                        ":--:  :--:  :  :--:--:  :  :  :  :--:--:  :  :--:--:--:  :  :\n" +
-                        "I     I     I     I     I  I  I           I  I        I  I  I \n" +
-                        ":  :--:  :  :--:  :  :--:  :--:--:--:  :--:--:  :  :  :  :  :\n" +
-                        "I        I     I     I  I           I           I  I  I  I  I \n" +
-                        ":  :--:--:--:  :--:--:  :--:  :--:  :--:--:--:--:  :  :  :  :\n" +
-                        "I  I  I        I     I        I  I           I     I  I     I \n" +
-                        ":  :  :  :--:  :  :  :--:--:--:  :  :--:--:  :--:--:  :  :--:\n" +
-                        "I  I  I     I     I  I  I     I     I        I     I  I  I  I \n" +
-                        ":  :  :--:  :--:--:--:--:  :  :--:--:  :--:--:  :  :  :  :  :\n" +
-                        "I     I     I              I  I        I     I  I     I  I  I \n" +
-                        ":--:--:  :--:  :--:--:--:  :  :  :--:--:  :--:  :--:  :  :  :\n" +
-                        "I     I     I        I  I  I     I           I     I  I     I \n" +
-                        ":  :  :--:--:--:--:  :  :  :--:--:  :--:--:  :--:  :--:--:--:\n" +
-                        "I  I           I        I  I     I        I        I        I \n" +
-                        ":  :--:--:--:  :  :--:--:  :  :  :--:--:  :--:--:--:  :--:  :\n" +
-                        "I  I     I     I  I     I     I     I     I     I  I  I     I \n" +
-                        ":  :--:  :  :--:  :--:  :--:--:--:--:  :  :  :  :  :  :  :  :\n" +
-                        "I        I              I        I     I     I  I  I  I  I  I \n" +
-                        ":--:--:--:--:--:--:--:--:  :--:  :  :--:--:--:  :  :  :  :--:\n" +
-                        "I           I     I           I  I        I        I  I     I \n" +
-                        ":  :--:--:  :  :  :  :--:--:--:  :--:  :  :--:--:--:  :  :  :\n" +
-                        "I     I  I  I  I     I           I     I              I  I  I \n" +
-                        ":--:  :  :  :  :  :--:  :--:--:--:  :  :--:--:--:--:--:--:  :\n" +
-                        "I  I     I     I  I           I     I  I        I           I \n" +
-                        ":  :  :--:--:--:  :  :--:--:  :  :--:  :  :--:--:  :--:--:--:\n" +
-                        "I  I  I  I        I  I     I  I     I     I     I  I        I \n" +
-                        ":  :  :  :  :  :--:  :  :  :--:  :  :--:--:  :  :  :--:--:  :\n" +
-                        "I  I     I  I  I  I     I        I           I  I        I  I \n" +
-                        ":  :--:--:  :  :  :  :--:--:--:--:  :--:--:--:--:--:  :  :  :\n" +
-                        "I     I     I     I     I        I  I     I     I     I     I \n" +
-                        ":--:  :  :--:  :--:--:  :  :--:  :--:  :  :  :  :  :--:--:--:\n" +
-                        "I     I     I  I     I  I  I     I     I  I  I  I     I     I \n" +
-                        ":  :--:  :  :--:  :  :  :--:  :  :  :--:  :  :  :--:  :  :  :\n" +
-                        "I        I        I  I        I     I     I        I     I  I \n" +
-                        ":--:--:--:--:--:--:--:--:--:--:--:--:--:--:--:--:--:--:--:  :\n";
+    @Test
+    public void test() throws IOException, URISyntaxException {
+        Amazing.random = new Random(seed);
+        Amazing.doit(colCount, rowCount);
 
-        Amazing.random = new Random(42);
-        Amazing.doit(20, 20);
-
-        assertEquals("Should have the maze that was expected", expected, Amazing.result.toString());
-    }
-
-    public void testSeed100size4x5() {
-        String expected =
-                "Amazing - Copyright by Creative Computing, Morristown, NJ\n" +
-                ":--:--:  :--:\n" +
-                "I     I     I \n" +
-                ":  :--:  :  :\n" +
-                "I  I     I  I \n" +
-                ":  :  :--:  :\n" +
-                "I  I  I     I \n" +
-                ":  :  :  :  :\n" +
-                "I  I  I  I  I \n" +
-                ":  :--:  :  :\n" +
-                "I  I  I  I  I \n" +
-                ":--:--:  :--:\n";
-
-        Amazing.random = new Random(100);
-        Amazing.doit(4, 5);
-        assertEquals("Should have the maze that was expected", expected, Amazing.result.toString());
+        assertEquals("Should have the maze that was expected", getExpectedResult(), Amazing.result.toString());
 
     }
 
-    public void testSeed50size4x5() {
-        String expected =
-                "Amazing - Copyright by Creative Computing, Morristown, NJ\n" +
-                        ":--:--:  :--:\n" +
-                        "I     I     I \n" +
-                        ":  :--:--:  :\n" +
-                        "I  I     I  I \n" +
-                        ":  :  :  :  :\n" +
-                        "I  I  I  I  I \n" +
-                        ":  :  :  :  :\n" +
-                        "I  I  I  I  I \n" +
-                        ":  :  :  :  :\n" +
-                        "I     I     I \n" +
-                        ":--:--:--:--:\n";
-
-        Amazing.random = new Random(50);
-        Amazing.doit(4, 5);
-        assertEquals("Should have the maze that was expected", expected, Amazing.result.toString());
-
-    }
-
-    public void testSeed0size1x1() {
-        String expected =
-                "Amazing - Copyright by Creative Computing, Morristown, NJ\n";
-
-        Amazing.random = new Random(0);
-        Amazing.doit(1, 1);
-        assertEquals("Should have the maze that was expected", expected, Amazing.result.toString());
-
+    private String getExpectedResult() throws URISyntaxException, IOException {
+        String fileName = "testSeed" + seed + "size" + colCount + "x" + rowCount;
+        try {
+            Path path = Paths.get(TestAmazing.class.getResource("expectedResults/" + fileName).toURI());
+            return new String(Files.readAllBytes(path), "UTF-8");
+        } catch (NullPointerException e) {
+            //result file does not exist
+            Path path = Paths.get(TestAmazing.class.getResource("expectedResults/").toURI()).resolve(fileName);
+            Files.write(path, Amazing.result.toString().getBytes("UTF-8"));
+            fail("Result file did not exist. It was created and has to be added to expectedResultFolder after validation by human being");
+            return null;
+        }
     }
 }
